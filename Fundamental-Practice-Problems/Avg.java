03401be94a0ed5be2725c3e53581ec473472d4f7
JAGDISH <js8724285@gmail.com>
2025-08-13 14:45:45 +0530

    commit

--- Fundamental-Practice-Problems/Avg.java
+++ Fundamental-Practice-Problems/Avg.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Average {
+public class Avg{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
