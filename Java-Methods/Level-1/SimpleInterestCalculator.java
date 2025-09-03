66b28987f3b750c93d50ab68ed415afd5e872f77
JAGDISH <js8724285@gmail.com>
2025-09-03 13:42:41 +0530

    [Jagdish] add . SimpleInterestCalculator

---
+++ Java-Methods/Level-1/SimpleInterestCalculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SimpleInterestCalculator {
+    public static double calculate(double principal, double rate, double time) {
+        return (principal * rate * time) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Principal, Rate, Time: ");
+        double p = sc.nextDouble(), r = sc.nextDouble(), t = sc.nextDouble();
+        double si = calculate(p, r, t);
+        System.out.println("The Simple Interest is " + si + " for Principal " + p + ", Rate of Interest " + r + " and Time " + t);
+    }
+}
