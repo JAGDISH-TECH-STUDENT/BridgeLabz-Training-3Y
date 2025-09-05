78ca8a79faf2181fa7c84da40838e14735b6e7d5
JAGDISH <js8724285@gmail.com>
2025-09-05 22:12:45 +0530

    [Jagdish] add. A program to generate a six-digit OTP number using Math.random() method.

---
+++ Java-Methods/Level-3/OTPGenerator.java
@@ -0,0 +1,37 @@
+public class OTPGenerator {
+
+    public static int generateOTP() {
+        return (int)(Math.random() * 900000) + 100000; // ensures 6-digit number
+    }
+
+    public static int[] generateMultipleOTPs(int count) {
+        int[] otps = new int[count];
+        for (int i = 0; i < count; i++) {
+            otps[i] = generateOTP();
+        }
+        return otps;
+    }
+
+    public static boolean areOTPsUnique(int[] otps) {
+        for (int i = 0; i < otps.length; i++) {
+            for (int j = i + 1; j < otps.length; j++) {
+                if (otps[i] == otps[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] otpArray = OTPGenerator.generateMultipleOTPs(10);
+
+        System.out.println("Generated OTPs:");
+        for (int otp : otpArray) {
+            System.out.println(otp);
+        }
+
+        boolean unique = OTPGenerator.areOTPsUnique(otpArray);
+        System.out.println("\nAre all OTPs unique? " + unique);
+    }
+}
+
