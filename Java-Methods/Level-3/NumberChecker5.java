88c63fe564352135319ef3c8dc9d9921e39d612f
JAGDISH <js8724285@gmail.com>
2025-09-05 22:05:09 +0530

    [Jagdish] add. NumberChecker5 utility class

---
+++ Java-Methods/Level-3/NumberChecker5.java
@@ -0,0 +1,53 @@
+public class NumberChecker5 {
+
+    public static boolean isPerfect(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) sum += i;
+        }
+        return sum == number;
+    }
+
+    public static boolean isAbundant(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) sum += i;
+        }
+        return sum > number;
+    }
+
+    public static boolean isDeficient(int number) {
+        int sum = 0;
+        for (int i = 1; i <= number / 2; i++) {
+            if (number % i == 0) sum += i;
+        }
+        return sum < number;
+    }
+
+    public static boolean isStrong(int number) {
+        int sum = 0;
+        int temp = number;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += factorial(digit);
+            temp /= 10;
+        }
+        return sum == number;
+    }
+
+    private static int factorial(int n) {
+        int fact = 1;
+        for (int i = 2; i <= n; i++) fact *= i;
+        return fact;
+    }
+    public static void main(String[] args) {
+        int number = 145;
+
+        System.out.println("Number: " + number);
+        System.out.println("Perfect Number: " + NumberChecker5.isPerfect(number));
+        System.out.println("Abundant Number: " + NumberChecker5.isAbundant(number));
+        System.out.println("Deficient Number: " + NumberChecker5.isDeficient(number));
+        System.out.println("Strong Number: " + NumberChecker5.isStrong(number));
+    }
+}
+
