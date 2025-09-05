21fd4e6f7cba1cf5af36d09dfa946f36fc6358a2
JAGDISH <js8724285@gmail.com>
2025-09-05 21:16:09 +0530

    [Jagdish] add. Vehicle Registration System

---
+++ KeyWords/Level-1/VehicleMain.java
@@ -0,0 +1,14 @@
+
+public class VehicleMain {
+    public static void main(String[] args) {
+        Vehicle.updateRegistrationFee(150.0);
+
+        Vehicle vehicle1 = new Vehicle("Honest Raj", "Sedan", "ABC123");
+        Vehicle vehicle2 = new Vehicle("Price Danish", "SUV", "XYZ789");
+
+        vehicle1.displayRegistrationDetails();
+        System.out.println();
+        vehicle2.displayRegistrationDetails();
+    }
+}
+
