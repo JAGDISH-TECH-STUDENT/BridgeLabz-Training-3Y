3f43f1c7900f40165fbbb9fd6af148c267d8a4d0
JAGDISH <js8724285@gmail.com>
2025-09-12 22:36:22 +0530

    [Jagdish] add. Smart Home Devices

---
+++ inheritance/SmartHomeTest.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class SmartHomeTest {
+    public static void main(String[] args) {
+        Thermostat t1 = new Thermostat("TH-101", "ON", 22.5);
+        t1.displayStatus();
+    }
+}
+
---
+++ inheritance/Thermostat.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class Thermostat extends Device {
+    double temperatureSetting;
+
+    public Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    public void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
