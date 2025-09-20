0257b89fa477d92dcf70654c93ecf6824dd1c57e
JAGDISH <js8724285@gmail.com>
2025-09-20 14:53:25 +0530

    [Jagdish] add. Check two Strings are equal

---
+++ Java-String/function.string/AnagramChecker.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static boolean check(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        char[] a1 = s1.toCharArray();
+        char[] a2 = s2.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        return Arrays.equals(a1, a2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Are Anagrams: " + check("listen", "silent"));
+    }
+}
+
