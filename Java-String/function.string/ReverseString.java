1e3d4197975e749a0def53931b45bff217f1d5b7
JAGDISH <js8724285@gmail.com>
2025-09-20 14:31:20 +0530

    Reversed the Given String

---
+++ Java-String/function.string/ReverseString.java
@@ -0,0 +1,14 @@
+
+public class ReverseString {
+    public static String reverse(String input) {
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--)
+            reversed += input.charAt(i);
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Reversed: " + reverse("Hello World"));
+    }
+}
+
